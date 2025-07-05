a349b7651c65ccd9f1d2c2dc12bbde89f7f64810
Peterjoram37 <videohidden21@gmail.com>
2025-07-05 12:06:20 +0000

    main

--- main.py
+++ main.py
@@ -38,5 +38,9 @@ def sms_reply():
     sms.send(reply, [sender], from_="+255753033342")
     return "OK", 200
 
+@app.route("/", methods=["GET"])
+def home():
+    return "SMS Bot is running!", 200
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
